6711bc9e2dd6e1f6bd29f0a51256e4b2954f8fd0
Eric Vandecasteele <Eric@Guriri>
2012-05-13 16:23:43 +0200

    update widget in service

--- src/fr/free/onlinux/AndroidBatteryWidget/AndroidBatteryWidgetProvider.java
+++ src/fr/free/onlinux/AndroidBatteryWidget/AndroidBatteryWidgetProvider.java
@@ -38,7 +38,7 @@ public class AndroidBatteryWidgetProvider extends AppWidgetProvider {
 		if (actionName.equals(Intent.ACTION_BATTERY_CHANGED)) {						
 			int rawlevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1); 						
 			if (rawlevel != rawlevelOld) {  			
-				updateWidget(context, intent);
+				//updateWidget(context, intent);
 				Intent service = new Intent(context, BatteryIntentService.class);
 				service.putExtras(intent);
 				context.startService(service); // update DB SQlite
@@ -52,37 +52,7 @@ public class AndroidBatteryWidgetProvider extends AppWidgetProvider {
 			int[] appWidgetIds) {
 		Log.i(TAG,"---------- onUpdate");
 		context.getApplicationContext().registerReceiver(this,
-				new IntentFilter(Intent.ACTION_BATTERY_CHANGED));		
-	}
-
-
-	public void updateWidget(Context context, Intent batteryIntent){
-		Log.i(TAG,"---------- updateWidget");
-		RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.androidbatterywidget_layout);
-		updateViews.setTextViewText(R.id.level, "waiting!");
-
-		int rawlevel = batteryIntent.getIntExtra("level", -1);
-		double scale = batteryIntent.getIntExtra("scale", -1);
-		double level = -1;
-		if (rawlevel >= 0 && scale > 0) {
-			level = rawlevel  * 100 / scale;
-		}
-		updateViews.setTextViewText(R.id.level, "" + level + " %" );
-		updateViews.setTextViewText(R.id.time, formatter.format(new Date()));
-		int temperature = batteryIntent.getIntExtra("temperature", -1);
-		String tempString= String.format("%.1f°C", new Float(temperature/10));
-		Log.d(TAG,"BAT:" + tempString + " " + level + "%");
-		updateViews.setTextViewText(R.id.temperature, tempString );
-		int voltage = batteryIntent.getIntExtra("voltage", -1);
-		updateViews.setTextViewText(R.id.voltage, "" + voltage + " mV" );
-
-		updateViews.setOnClickPendingIntent(R.id.layout ,
-				PendingIntent.getActivity(context, 0, 
-						new Intent(context, AndroidBatteryActivity.class),Intent.FLAG_ACTIVITY_NEW_TASK));
-
-		ComponentName myComponentName = new ComponentName(context, AndroidBatteryWidgetProvider.class);
-		AppWidgetManager manager = AppWidgetManager.getInstance(context);
-		manager.updateAppWidget(myComponentName, updateViews);
+				new IntentFilter(Intent.ACTION_BATTERY_CHANGED));	
 	}
 
 	public void onDisabled(Context context) {
@@ -90,11 +60,6 @@ public class AndroidBatteryWidgetProvider extends AppWidgetProvider {
 		Log.i(TAG,"---------- onDisabled ");		
 		super.onDisabled(context);
 
-	}
-	public void onDeleted(Context context, int[] appWidgetIds) {
-		// TODO Auto-generated method stub
-		//super.onDeleted(context, appWidgetIds);
-		Log.i(TAG,"---------- onDeleted ");
 	}
 
 }
--- src/fr/free/onlinux/AndroidBatteryWidget/BatteryIntentService.java
+++ src/fr/free/onlinux/AndroidBatteryWidget/BatteryIntentService.java
@@ -1,10 +1,17 @@
 package fr.free.onlinux.AndroidBatteryWidget;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 import android.app.IntentService;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.BatteryManager;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 
 public class BatteryIntentService extends IntentService {
@@ -16,6 +23,9 @@ public class BatteryIntentService extends IntentService {
 	
 	@Override
 	protected void onHandleIntent(Intent intent) {
+		//First update widget views
+		updateWidget(getApplicationContext(), intent);
+		//Second, update database
 		Log.i(TAG, "BAT onHandleIntent: " + intent.getExtras());
 		int rawlevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);        		
 		int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
@@ -36,6 +46,36 @@ public class BatteryIntentService extends IntentService {
 		Log.i( TAG, "---------- Add record: " + batterylevel + " time: "+ Calendar.getInstance().getTimeInMillis() );
 		
 
+	}
+	
+	public void updateWidget(Context context, Intent batteryIntent){
+		Log.i(TAG,"---------- updateWidget");
+		SimpleDateFormat formatter = new SimpleDateFormat(" HH:mm:ss ");
+		RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.androidbatterywidget_layout);
+		updateViews.setTextViewText(R.id.level, "waiting!");
+
+		int rawlevel = batteryIntent.getIntExtra("level", -1);
+		double scale = batteryIntent.getIntExtra("scale", -1);
+		double level = -1;
+		if (rawlevel >= 0 && scale > 0) {
+			level = rawlevel  * 100 / scale;
+		}
+		updateViews.setTextViewText(R.id.level, "" + level + " %" );
+		updateViews.setTextViewText(R.id.time, formatter.format(new Date()));
+		int temperature = batteryIntent.getIntExtra("temperature", -1);
+		String tempString= String.format("%.1f°C", new Float(temperature/10));
+		Log.d(TAG,"BAT:" + tempString + " " + level + "%");
+		updateViews.setTextViewText(R.id.temperature, tempString );
+		int voltage = batteryIntent.getIntExtra("voltage", -1);
+		updateViews.setTextViewText(R.id.voltage, "" + voltage + " mV" );
+
+		updateViews.setOnClickPendingIntent(R.id.layout ,
+				PendingIntent.getActivity(context, 0, 
+						new Intent(context, AndroidBatteryActivity.class),Intent.FLAG_ACTIVITY_NEW_TASK));
+
+		ComponentName myComponentName = new ComponentName(context, AndroidBatteryWidgetProvider.class);
+		AppWidgetManager manager = AppWidgetManager.getInstance(context);
+		manager.updateAppWidget(myComponentName, updateViews);
 	}
 
 }
